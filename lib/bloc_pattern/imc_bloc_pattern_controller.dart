d5c6b5d2da690d1f584cdff62854408ffa50744e
leo <realleo2009@hotmail.com>
2022-01-05 17:32:16 -0300

    state

---
+++ lib/bloc_pattern/imc_bloc_pattern_controller.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:state_manager/bloc_pattern/imc_state.dart';
+
+class ImcBlocPatternController {
+  final _imcStreamController = StreamController<ImcState>()
+    ..add(ImcState(imc: 0));
+
+  Stream<ImcState> get imcOut => _imcStreamController.stream;
+
+  Future<void> calcularImc(
+      {required double peso, required double altura}) async {
+    _imcStreamController.add(ImcState(imc: 0));
+    await Future.delayed(const Duration(seconds: 1));
+    final imc = peso / pow(altura, 2);
+    _imcStreamController.add(ImcState(imc: imc));
+  }
+
+  void dispose() {
+    _imcStreamController.close();
+  }
+}
--- lib/bloc_pattern/imc_bloc_pattern_page.dart
+++ lib/bloc_pattern/imc_bloc_pattern_page.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:state_manager/bloc_pattern/imc_bloc_pattern_controller.dart';
+import 'package:state_manager/widgets/imc_gauge.dart';
 
 class ImcBlocPatternPage extends StatefulWidget {
   const ImcBlocPatternPage({Key? key}) : super(key: key);
@@ -8,8 +13,96 @@ class ImcBlocPatternPage extends StatefulWidget {
 }
 
 class _ImcBlocPatternPageState extends State<ImcBlocPatternPage> {
+  final controller = ImcBlocPatternController();
+  final pesoEC = TextEditingController();
+  final alturaEC = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+
+
+
+  @override
+  void dispose() {
+    pesoEC.dispose();
+    alturaEC.dispose();
+    controller.dispose();
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Imc SetState'),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              children: [
+                ImcGauge(imc: 0),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: pesoEC,
+                  decoration: const InputDecoration(labelText: 'Peso'),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [
+                    CurrencyTextInputFormatter(
+                      locale: 'pt_BR',
+                      symbol: '',
+                      decimalDigits: 2,
+                      turnOffGrouping: true,
+                    ),
+                  ],
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Campo Obrigatorio';
+                    }
+                  },
+                ),
+                TextFormField(
+                  controller: alturaEC,
+                  decoration: const InputDecoration(labelText: 'Altura'),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [
+                    CurrencyTextInputFormatter(
+                      locale: 'pt_BR',
+                      symbol: '',
+                      decimalDigits: 2,
+                      turnOffGrouping: true,
+                    ),
+                  ],
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Campo Obrigatorio';
+                    }
+                  },
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    var formValid = formKey.currentState?.validate() ?? false;
+
+                    if (formValid) {
+                      var formatter = NumberFormat.simpleCurrency(
+                          locale: 'pt_BR', decimalDigits: 2);
+                      double peso = formatter.parse(pesoEC.text) as double;
+                      double altura = formatter.parse(alturaEC.text) as double;
+
+                      //_calcularImc(peso: peso, altura: altura);
+                    }
+                  },
+                  child: const Text('Calcular IMC'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+
   }
 }
